bfd7742b69ed547eaf97aa257e7b1ff72ff5cf2f
Riboni.1989 <dedal89@gmail.com>
2022-02-10 19:20:56 +0100

    added phone number field for test

--- Configuration/Capabilities.js
+++ Configuration/Capabilities.js
@@ -1,6 +1,7 @@
 var capabilities = {
     "browser": "firefox",
-    "endpoint": "https://portal.staging.scalapay.com/"
+    "endpoint": "https://portal.staging.scalapay.com/",
+    "phoneNumber": "ADD_VALID_PHONE_NUMBER"
 }
 
 module.exports = {
